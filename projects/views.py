d79ebcf99fa3e693026cca14b142b716fdcd6414
itay moshel <itay@itays-MacBook-Pro.local>
2022-12-24 21:40:22 +0200

    Added projects reviews

--- projects/views.py
+++ projects/views.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .models import Project, Tag
-from .forms import ProjectForm
+from .forms import ProjectForm, ReviewForm
 from .utils import searchProject, paginateProjects
+from django.contrib import messages
 
 
 def projects(request):
@@ -19,7 +20,22 @@ def projects(request):
 
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
-    return render(request, "projects/single-project.html", {'project': projectObj})
+    form = ReviewForm()
+    
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        review = form.save(commit=False)
+        review.project = projectObj
+        review.owner = request.user.profile
+        review.save()
+        
+        projectObj.getVoteCount
+        
+        messages.success(request, 'Your review was successfully submitted!')
+        return redirect('project', pk=projectObj.id)
+        
+        
+    return render(request, "projects/single-project.html", {'project': projectObj, 'form':form, })
 
 @login_required(login_url="login")
 def createProject(request):
